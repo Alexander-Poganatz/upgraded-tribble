5110fc2dab2addd684f9704c690c683e4be0955f
Alexander-Poganatz <a_poganatz@outlook.com>
2023-01-24 07:41:09 -0500

    Added transfer procedure

---
+++ EnvelopeDbUp/Scripts/Always/513_transfer.sql
@@ -0,0 +1,30 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS transfer;
+CREATE PROCEDURE transfer(
+	IN uID INT UNSIGNED
+	,IN eSourceNumber SMALLINT UNSIGNED
+	,IN eDestinationNumber SMALLINT UNSIGNED
+	,IN amount INT SIGNED
+)
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		ROLLBACK;
+		RESIGNAL;
+	END;
+	
+	IF (SELECT COUNT(*) FROM ENVELOPE WHERE UserID = uID AND EnvelopeNumber IN (eSourceNumber, eDestinationNumber)) <> 2 THEN
+		SELECT 0;
+	ELSE
+		START TRANSACTION;
+	
+		CALL ins_EnvelopeTransaction(uID, eSourceNumber, ABS(amount) * -1, UTC_DATE(),CONCAT('Transfer to ', (SELECT EnvelopeName FROM envelope WHERE UserID = uID AND EnvelopeNumber = eDestinationNumber)));
+		CALL ins_EnvelopeTransaction(uID, eDestinationNumber, ABS(amount), UTC_DATE(),CONCAT('Transfer from ', (SELECT EnvelopeName FROM envelope WHERE UserID = uID AND EnvelopeNumber = eSourceNumber)));
+		COMMIT;
+		SELECT 1;
+	END IF;
+	
+END //
+
+DELIMITER ;

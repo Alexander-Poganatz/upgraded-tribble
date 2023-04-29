e09c95167f6f19574cb1523271c7cf85275162a5
Alexander-Poganatz <a_poganatz@outlook.com>
2023-04-29 13:42:41 -0400

    Put in missing sql change from pagination

--- EnvelopeDbUp/Scripts/Always/511_sel_Transactions.sql
+++ EnvelopeDbUp/Scripts/Always/511_sel_Transactions.sql
@@ -4,12 +4,20 @@ DROP PROCEDURE IF EXISTS sel_Transactions;
 CREATE PROCEDURE sel_Transactions(
 	IN uID INT UNSIGNED
 	,IN eNumber SMALLINT UNSIGNED
+	,IN limitNum INT UNSIGNED
+	,IN offsetNum INT UNSIGNED
 )
 BEGIN
 	SELECT t.TransactionNumber, t.TransactionAmount, t.TransactionDate, t.Note
 	FROM envelope e
 	INNER JOIN envelopetransaction t ON e.EnvelopeID = t.EnvelopeID
+	WHERE UserID = uID AND EnvelopeNumber = eNumber
+	LIMIT limitNum OFFSET offsetNum;
+	
+	SELECT COUNT(*) AS NumberOfTransactions
+	FROM envelope e
+	INNER JOIN envelopetransaction t ON e.EnvelopeID = t.EnvelopeID
 	WHERE UserID = uID AND EnvelopeNumber = eNumber;
 END //
 
-DELIMITER ;envelopetransaction
+DELIMITER ;

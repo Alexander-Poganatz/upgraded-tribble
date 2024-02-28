7e370595beeaa327f757d69a0e796c4fd0454ad9
Alexander-Poganatz <a_poganatz@outlook.com>
2024-02-28 17:43:28 -0500

    Added permission policy and removed server header

--- EnvelopeWeb/CustomMiddleware.fs
+++ EnvelopeWeb/CustomMiddleware.fs
@@ -2,6 +2,7 @@
 
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Primitives
+open System.Collections.Generic
 
 let private contentSecurityPolicy =
 #if DEBUG
@@ -10,8 +11,14 @@ let private contentSecurityPolicy =
     StringValues("default-src 'self';")
 #endif
 
+// copied from owasp.org
+let private permissionPolicy =
+    StringValues("accelerometer=(),ambient-light-sensor=(),autoplay=(),battery=(),camera=(),display-capture=(),document-domain=(),encrypted-media=(),fullscreen=(),gamepad=(),geolocation=(),gyroscope=(),layout-animations=(),legacy-image-formats=(),magnetometer=(),microphone=(),midi=(),oversized-images=(),payment=(),picture-in-picture=(),publickey-credentials-get=(),speaker-selection=(),sync-xhr=(),unoptimized-images=(),unsized-media=(),usb=(),screen-wake-lock=(),web-share=(),xr-spatial-tracking=()")
+
+let private permissionPolicyPair = KeyValuePair.Create("Permissions-Policy", permissionPolicy)
 // Adds headers for all web pages
 let addHeaderMiddleWare (context: HttpContext) (next: RequestDelegate) =
     context.Response.Headers.ContentSecurityPolicy <- contentSecurityPolicy;
+    context.Response.Headers.Add(permissionPolicyPair);
 
     next.Invoke(context)

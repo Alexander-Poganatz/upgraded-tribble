df38f11786f0b870cf88ee6f607ef6504a9b86a9
Alexander-Poganatz <a_poganatz@outlook.com>
2024-02-29 21:11:08 -0500

    Added a couple more headers

--- EnvelopeWeb/CustomMiddleware.fs
+++ EnvelopeWeb/CustomMiddleware.fs
@@ -15,10 +15,17 @@ let private contentSecurityPolicy =
 let private permissionPolicy =
     StringValues("accelerometer=(),ambient-light-sensor=(),autoplay=(),battery=(),camera=(),display-capture=(),document-domain=(),encrypted-media=(),fullscreen=(),gamepad=(),geolocation=(),gyroscope=(),layout-animations=(),legacy-image-formats=(),magnetometer=(),microphone=(),midi=(),oversized-images=(),payment=(),picture-in-picture=(),publickey-credentials-get=(),speaker-selection=(),sync-xhr=(),unoptimized-images=(),unsized-media=(),usb=(),screen-wake-lock=(),web-share=(),xr-spatial-tracking=()")
 
+let deny = StringValues("DENY")
+
+let nosniff = StringValues("nosniff")
+
+
 let private permissionPolicyPair = KeyValuePair.Create("Permissions-Policy", permissionPolicy)
 // Adds headers for all web pages
 let addHeaderMiddleWare (context: HttpContext) (next: RequestDelegate) =
     context.Response.Headers.ContentSecurityPolicy <- contentSecurityPolicy;
     context.Response.Headers.Add(permissionPolicyPair);
+    context.Response.Headers.XFrameOptions <- deny;
+    context.Response.Headers.XContentTypeOptions <- nosniff;
 
     next.Invoke(context)
